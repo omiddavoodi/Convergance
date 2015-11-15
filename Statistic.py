ad91f8c5fcfffb61d53c4d7d395a1f313e284c51
hooman <behnejad@comp.iust.ac.ir>
2015-11-15 21:29:06 +0330

    add delay window count

--- Statistic.py
+++ Statistic.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     }
     queue = []
     totalQueue = 0
+    delayQueue = []
 
     startTick = 0
     endTick = 0
@@ -39,6 +40,7 @@ if __name__ == "__main__":
 
             elif line[1][0] == 'Dequeued':
                 totalQueue += float(line[-1]) - queue[0]
+                delayQueue.append(float(line[-1]) - queue[0])
                 del queue[0]
 
         elif line[0] == 'Simulation started':
@@ -49,6 +51,23 @@ if __name__ == "__main__":
 
     print('Simulation Time: ' + str(endTick - startTick))
     print('Total Queue Time: ' + str(totalQueue))
+
+    delayQueue = sorted(delayQueue)
+
+    s = 0
+    step = 10
+    index = 0
+    tmp = 0
+    while index < len(delayQueue):
+        if s <= delayQueue[index] <= (s + step):
+            tmp += 1
+
+        else:
+            print("N.O. of Delay in [%d, %d): %d" % (s, s + step, tmp))
+            s += step
+            tmp = 0
+
+        index += 1
 
     for i in entity:
         print('Utilization for ' + i + ": " + str(sum(entity[i])/(endTick - startTick)) + " : " + str(len(entity[i])))
--- able-baker.py
+++ able-baker.py
@@ -5,7 +5,7 @@ from convergance_actor_branch import Branch
 from convergance_actor_delay import Delay
 from convergance_actor_queue import Queue
 from convergance_entity import Entity
-from randomNumber import NORMAL, UNIFORM, RANDINT, GAMMA, NumberGenerator
+from randomNumber import NORMAL, UNIFORM, RANDINT, TRIANGULAR, NumberGenerator
 
 # Entity Generation Random
 INTERVAL_RATE = NumberGenerator(NORMAL, mu=1, sigma=3)
@@ -13,7 +13,7 @@ INTERVAL_RATE = NumberGenerator(NORMAL, mu=1, sigma=3)
 
 # Delay Random
 DELAY_RATE_BAKER = NumberGenerator(UNIFORM, a=1, b=5)
-DELAY_RATE_ABLE = NumberGenerator(GAMMA, alpha=1, beta=0.5)
+DELAY_RATE_ABLE = NumberGenerator(TRIANGULAR, low=1, high=6, mode=5)
 
 # DELAY_RATE_BAKER = NumberGenerator(RANDINT, a=1, b=7)
 # DELAY_RATE_ABLE = NumberGenerator(RANDINT, a=1, b=5)
@@ -34,7 +34,7 @@ dl2.logs = True
 q1 = Queue([dl1, dl2])
 q1.logs = True
 
-g1 = Generator(q1, Entity, None, INTERVAL_RATE.next, 50)
+g1 = Generator(q1, Entity, None, INTERVAL_RATE.next, 500)
 g1.logs = True
 
 sim = Simulation()
